57092f7552275fcfd8334fd7702b3c2390cb75f9
nulzo <nolanpgregory@gmail.com>
2025-12-07 13:10:25 -0600

    [add] - day07 part 2; pretty tough to figure out

--- day07/day07.py
+++ day07/day07.py
@@ -15,14 +15,30 @@ def part1(data):
         nxt = set()
         for c in cols:
             ok = g[r][c] != '^'
-            nxt.add(c) if ok else nxt.update([c - 1, c + 1])
-            if ok: tot += 1
+            nxt.add(c) if ok else nxt.update([c-1,c+1])
+            if not ok: tot += 1
         cols = nxt
     return tot
 
 
 def part2(data):
-    return 0
+    g = [list(line) for line in data.split('\n')]
+    start = -1
+    for c in range(len(g[0])):
+        if g[0][c] == 'S':
+            start = c
+    cts = {start: 1}
+    tlines = 0
+    for r in range(len(g)):
+        nxt = defaultdict(int)
+        for c, pths in cts.items():
+            tgs = [c - 1, c + 1] if g[r][c] == '^' else [c]
+            for tg in tgs:
+                if tg < 0 or tg >= len(g[0]): tlines += pths
+                else: nxt[tg] += pths
+        cts = nxt
+    tlines += sum(cts.values())
+    return tlines
 
 
 if __name__ == "__main__":
